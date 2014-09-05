c2be971611aba60831c0b66612f35008745a6ed9
Liam Lagay <liam.lagay@sage.com>
2014-09-05 13:37:48 +0100

    tidying up changes

---
+++ UserStory3/file_output.rb
@@ -0,0 +1,3 @@
+class FileOutput
+
+end
---
+++ specs/file_output_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require_relative '../UserStory3/file_output'
+
+describe FileOutput do
+  it 'should return a list of entries' do
+
+  end
+end
