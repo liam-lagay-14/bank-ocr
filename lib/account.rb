2802dd0b27e16f77d8bd6464e0369a91fd1d3219
Liam Lagay <liam.lagay@sage.com>
2014-09-08 09:05:10 +0100

    Refactoring account to set accountdetails in a seperate method

--- lib/account.rb
+++ lib/account.rb
@@ -8,17 +8,21 @@ class Account
     @account_number = account_number
     @account_checksum = AccountCheckSum.new(account_number)
 
-    @account_details = if account_number.include? '?' || !is_valid_account_number?
-                         'ILL'
-                       elsif !account_checksum.is_account_valid_checksum?
-                         'ERR'
-                       else ''
-                       end
+    @account_details = set_account_details
   end
 
   private
 
   def is_valid_account_number?
     (account_number =~ /^[0-9]{9}$/) == 0
+  end
+
+  def set_account_details
+    if account_number.include? '?' || !is_valid_account_number?
+                    'ILL'
+                  elsif !account_checksum.is_account_valid_checksum?
+                    'ERR'
+                  else ''
+    end
   end
 end
